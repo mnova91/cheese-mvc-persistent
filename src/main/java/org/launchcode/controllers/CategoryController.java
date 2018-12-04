b3a2c6d1c442701ae9833bacec3e3305aa0ee0e2
Michael Novakoski <michaelnovakoski@live.com>
2018-12-03 20:30:50 -0500

    studio 1

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,12 +1,18 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.Category;
+import org.launchcode.models.Cheese;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "category")
@@ -21,14 +27,16 @@ public class CategoryController {
         templateVariables.addAttribute("categories", categoryDao.findAll());
         return "category/index"; }
 
-//    @RequestMapping(value = "add")
-//    public String add() {
-//        //render an add form
-//        return ""; }
-//
-//    @RequestMapping(value = "add", method = RequestMethod.POST)
-//    public String add() {
-//        //accepts POST form request
-//        return ""; }
+    @RequestMapping(value = "add")
+    public String add(Model templateVariables) {
+        templateVariables.addAttribute("title", "Add Category");
+        templateVariables.addAttribute("category", new Category());
+        return "category/add"; }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(@ModelAttribute @Valid Category category,
+                      Errors errors, Model model) {
+        categoryDao.save(category);
+        return "redirect:"; }
 
 }
